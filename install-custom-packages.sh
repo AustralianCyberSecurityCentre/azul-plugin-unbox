2fe10303b607791a1b61160023c2b3b2b7939c42
dan-acsc <dan-acsc@users.noreply.github.com>
2026-03-18 21:04:42 +0000

    feat: upgrade upx and 7zip.

--- install-custom-packages.sh
+++ install-custom-packages.sh
@@ -5,7 +5,7 @@ set -e
 # Install latest version of 7 zip. (run with sudo)
 # refer to https://www.7-zip.org/download.html and get the 64bit linux x86 link for newest version.
 # last updated 16-jun-2025
-wget https://www.7-zip.org/a/7z2409-linux-x64.tar.xz
+wget https://www.7-zip.org/a/7z2600-linux-x64.tar.xz
 # Remove previous install
 rm -rf /opt/7z
 rm -f /usr/bin/7zzs
@@ -18,8 +18,8 @@ ln -s /opt/7z/7zzs /usr/bin/7zzs
 # Install latest version of UPX. (run with sudo)
 # refer to https://github.com/upx/upx/releases for updates (amd64_linux.tar version)
 # last updated 16-jun-2025
-wget -qO- https://github.com/upx/upx/releases/download/v5.0.1/upx-5.0.1-amd64_linux.tar.xz | unxz | tar -x
-mv upx-5.0.1-amd64_linux/upx /usr/local/bin
+wget -qO- https://github.com/upx/upx/releases/download/v5.1.1/upx-5.1.1-amd64_linux.tar.xz | unxz | tar -x
+mv upx-5.1.1-amd64_linux/upx /usr/local/bin
 chmod +x /usr/local/bin/upx
-rm -r upx-5.0.1-amd64_linux
+rm -r upx-5.1.1-amd64_linux
 echo "UPX version $(upx --version)"
